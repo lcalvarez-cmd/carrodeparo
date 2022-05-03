679d647677c3a9dfe4c5a3975182091ba0a45779
lcalvarez-cmd <lcalvarez@unicauca.edu.co>
2022-05-02 23:24:01 -0500

    carro de paro

---
+++ src/main/java/com/hospitalsanjose/gov/co/CarroDeParoApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalsanjose.gov.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroDeParoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroDeParoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitalsanjose/gov/co/CarroDeParoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalsanjose.gov.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroDeParoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
